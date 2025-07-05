68b684d1bb9214de2c24d90bea04382fa9ca408f
Mårten Blankfors <marten@blankfors.se>
2025-07-05 11:30:56 +0200

    fix persistent clipboard

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,13 @@
+use std::process::Command;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 async fn copy_to_clipboard(text: String, app_handle: tauri::AppHandle) -> Result<(), String> {
-    use tauri_plugin_clipboard_manager::ClipboardExt;
+    Command::new("wl-copy")
+        .arg(text)
+        .output()
+        .expect("failed to execute process");
 
-    app_handle
-        .clipboard()
-        .write_text(text)
-        .map_err(|e| e.to_string())?;
-
-    // Close the application after copying
     app_handle.exit(0);
 
     Ok(())
@@ -18,7 +17,6 @@ async fn copy_to_clipboard(text: String, app_handle: tauri::AppHandle) -> Result
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_clipboard_manager::init())
         .invoke_handler(tauri::generate_handler![copy_to_clipboard])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
